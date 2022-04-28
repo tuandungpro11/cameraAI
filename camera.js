44f55b17830bd8b51cc20abf674381b830ca5de1
tuandungpro11 <52460184+tuandungpro11@users.noreply.github.com>
2022-04-28 08:54:24 +0700

    Update camera.js

--- camera.js
+++ camera.js
@@ -3,7 +3,7 @@ var requestOptions = {
     redirect: 'follow'
   };
   
-  fetch("https://oauth.hanet.com/oauth2/authorize?response_type=code&client_id=5e8cfe50de01b7fe082d662f029e80d5&redirect_uri=http://127.0.0.1:5500/index.html?code=AUTHORIZATION_CODE&scope=full", requestOptions)
+  fetch("https://oauth.hanet.com/oauth2/authorize?response_type=code&client_id=5e8cfe50de01b7fe082d662f029e80d5&redirect_uri=https://www.tvsi.com.vn/?msclkid=4e8d9c9fc69011ecb079a65d8e8c5826?code=AUTHORIZATION_CODE&scope=full", requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
